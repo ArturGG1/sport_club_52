5938c87fc7a5548225a0956ba095d929516a4e13
ArturGG1 <a.khusainov11022007@gmail.com>
2025-01-31 19:41:45 +1000

    я хз лол

---
+++ Pages/Auth.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace sport_club_52.Pages;
+
+public partial class Auth : Page
+{
+    private MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+    public Auth()
+    {
+        InitializeComponent();
+    }
+    public void Button_Click(object sender, RoutedEventArgs e)
+    {
+        string ButtonName = ((Button)sender).Name;
+        string Action = "";
+        if (ButtonName.Contains("Create")) Action = "Create";
+        else Action = "Login";
+        switch (Action)
+        {
+            case "Create":
+            {
+                // TODO: добавить странички для создания
+                string UserType = ButtonName.Replace("Create", "");
+                switch (UserType)
+                {
+                    case "Trainer":
+                        break;
+                    case "Cashier":
+                        break;
+                    case "User":
+                        break;
+                }
+                break;
+            }
+            case "Login":
+            {
+                // TODO: добавить странички для входа
+                string UserType = ButtonName.Replace("Login", "");
+                switch (UserType)
+                {
+                    case "Trainer":
+                        break;
+                    case "Cashier":
+                        break;
+                    case "User":
+                        break;
+                }
+                break;
+            }
+        }
+    }
+}
