7e5ce806d1100d78a3c7ef5a47209ad14748c42b
ArturGG1 <a.khusainov11022007@gmail.com>
2025-01-31 18:18:49 +1000

    база данных работает!!!!

---
+++ Context.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace sport_club_52;
+
+public class SportClubContext : DbContext
+{
+    public DbSet<Tariff> Tariffs { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Membership> Memberships { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Trainer> Trainers { get; set; }
+    public DbSet<Cashier> Cashiers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=sportclub.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Tariff>()
+            .HasOne(t => t.Trainer)
+            .WithMany()
+            .HasForeignKey(t => t.TrainerId);
+        modelBuilder.Entity<Client>()
+            .HasOne(c => c.Membership)
+            .WithMany()
+            .HasForeignKey(c => c.MembershipId);
+        modelBuilder.Entity<Membership>()
+            .HasOne(m => m.Tariff)
+            .WithMany()
+            .HasForeignKey(m => m.TariffId);
+        modelBuilder.Entity<Membership>()
+            .HasOne(m => m.Payment)
+            .WithMany()
+            .HasForeignKey(m => m.PaymentId);
+        modelBuilder.Entity<Payment>()
+            .HasOne(p => p.Cashier)
+            .WithMany(c => c.Payments)
+            .HasForeignKey(p => p.CashierId);
+    }
+}
--- DataModel.cs
+++ DataModel.cs
@@ -5,10 +5,11 @@ public class Tariff
     public uint Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public uint TrainerId { get; set; } // внешний ключ на тренера
+    public Trainer? Trainer { get; set; }
     public byte Visits { get; set; }
     public uint Price { get; set; } 
 }
-
 public class Client
 {
     public uint Id { get; set; }
@@ -16,22 +17,39 @@ public class Client
     public DateOnly Birthday { get; set; }
     public string Phone { get; set; }
     public string Address { get; set; }
+    public uint MembershipId { get; set; } // внешний ключ на абонемент
     public Membership Membership { get; set; } // абонемент
     public uint BoxId { get; set; } // номер шкафа
 }
-
 public class Membership
 {
     public uint Id { get; set; }
+    public uint TariffId { get; set; } // внешний ключ на тариф
     public Tariff Tariff { get; set; }
+    public uint PaymentId { get; set; } // внешний ключ на оплату
     public Payment Payment { get; set; }
     public byte VisitsLeft { get; set; } // кол-во оставшихся занятий
     public bool Suspended { get; set; } // приостановлен ли абонемент
 }
-
 public class Payment
 {
     public uint Id { get; set; }
     public uint Amount { get; set; }
     public DateTime PaymentDate { get; set; }
+    public uint CashierId { get; set; } // внешний ключ на кассира
+    public Cashier Cashier { get; set; }
+}
+public abstract class Worker
+{
+    public uint Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+}
+public class Trainer : Worker
+{
+    
+}
+public class Cashier : Worker
+{
+    public List<Payment> Payments { get; set; }
 }
